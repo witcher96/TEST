e1049ffaaeb04eead7d430e4f7837f40367386d8
witcher96 <hasanaa@code.edu.az>
2022-01-13 17:02:20 +0400

    testrepo.bat filesi elave olundu

---
+++ testrepo.bat
@@ -0,0 +1,12 @@
+Title Matrix
+color 0a
+systeminfo
+cd desktop
+prompt #
+mode 1000
+pause
+@echo off
+:A
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto A
+
